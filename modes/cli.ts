4d8747473c6a5ddfd5e91ca824a596a24672a6e8
Hrishikesh Jha <jha.kharakh@gmail.com>
2026-06-04 08:48:51 +0530

    Created two more modes

--- modes/cli.ts
+++ modes/cli.ts
@@ -1,6 +1,8 @@
 import chalk from "chalk"
 import {select, isCancel} from "@clack/prompts"
 import { runAgentMode } from "./agent/orchestrator";
+import { runAskMode } from "./ask/orchestrator";
+import { runPlanMode } from "./plan/orchestrator";
 
 
 
@@ -22,10 +24,10 @@ export async function runCliMode(){
             await runAgentMode()
         }
         if(mode === "ask"){
-            console.log("ask")
+            await runAskMode()
         }
         if(mode === "plan"){
-            console.log("plan")
+            await runPlanMode()
         }
 
         if(mode !=="agent" && mode !=="plan" && mode!=="ask"){
